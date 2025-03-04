5d6589b33e9a1360f538866e198306fd8a7850fe
Aman14-ai <amancode2005@gmail.com>
2025-03-04 19:21:46 +0530

    Enhance button interactivity with cursor pointer style for better user experience

--- backgroundChanger/src/App.jsx
+++ backgroundChanger/src/App.jsx
@@ -11,19 +11,19 @@ function App() {
   <div className="fixed bottom-16 inset-x-0 flex justify-center">
     <div className="w-fit bg-green-100 px-4 py-2 rounded-3xl flex flex-wrap gap-3 shadow-xl sm:gap-5">
       <div className="text-amber-100 font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "green" }}>
-        <button onClick={() => setColor("green")} className='outline-none px-3'>Green</button>
+        <button onClick={() => setColor("green")} className='cursor-pointer outline-none px-3'>Green</button>
       </div>
       <div className="font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "white" }}>
-        <button onClick={() => setColor("white")} className='outline-none px-3'>White</button>
+        <button onClick={() => setColor("white")} className='cursor-pointer outline-none px-3'>White</button>
       </div>
-      <div className="text-amber-50 font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "red" }}>
-        <button onClick={() => setColor("red")} className='outline-none px-3'>Red</button>
+      <div className="cursor-pointer text-amber-50 font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "red" }}>
+        <button onClick={() => setColor("red")} className='cursor-pointer outline-none px-3'>Red</button>
       </div>
       <div className="text-yellow-50 font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "blue" }}>
-        <button onClick={() => setColor("blue")} className='outline-none px-3'>Blue</button>
+        <button onClick={() => setColor("blue")} className='outline-none px-3 cursor-pointer'>Blue</button>
       </div>
       <div className="text-white font-light rounded-xl p-2 text-xs shadow-md" style={{ backgroundColor: "olive" }}>
-        <button onClick={() => setColor("olive")} className='outline-none px-3'>Olive</button>
+        <button onClick={() => setColor("olive")} className='cursor-pointer outline-none px-3'>Olive</button>
       </div>
     </div>
   </div>
