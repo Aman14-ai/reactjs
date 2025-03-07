9cad13db63808d41e6a688db7bf875f9f1bd7b41
Aman14-ai <amancode2005@gmail.com>
2025-03-07 21:17:47 +0530

    Initialize React application with Vite, including configuration, styling, and basic components

--- freeCodeCamp/Random houseKeeping/src/Header.jsx
+++ freeCodeCamp/Random houseKeeping/src/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
       <div className="header">
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz34OmNVyIMh1rguNfXC3MBk7Qq3DTduJVVg&s' width={55} height={50} />
-        <h1 className='inline'>React Facts</h1>
+        <h1 className='inline'>ReactFacts</h1>
         <ul className='headerUL'>
           <li>Home</li>
           <li>About</li>
