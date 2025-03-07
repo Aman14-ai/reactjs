fa18651169be300c26549ba9a93b77ae28b5a7ab
Aman14-ai <amancode2005@gmail.com>
2025-03-08 01:01:38 +0530

    Add mapping functions for names and books; log capitalized names and HTML structure

--- freeCodeCamp/03React_TravelJournal/src/test.js
+++ freeCodeCamp/03React_TravelJournal/src/test.js
@@ -13,3 +13,13 @@ console.log(evenOdd);
 
 const objects = nums.map((num) => ({ number: num, isEven: num % 2 === 0 }));
 console.log(objects);
+
+const names = ['john', 'jacob', 'jingle', 'heimer', 'schmidt'];
+const upperNames = names.map((name) => name.toUpperCase());
+const capitalised = names.map((name) => name[0].toUpperCase() +" " + name.slice(1));
+console.log(capitalised);
+console.log(upperNames);
+
+const books = ['The Bible', 'The Quran', 'The Bhagavad Gita', 'The Torah'];
+const booksHTML = books.map((book) => `<h1>${book}</h1>`);
+console.log(booksHTML);
